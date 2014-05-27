23e2005ca991bbd35c693b38fdaa25c29d5d0d67
Michael Friedrich <michael.friedrich@netways.de>
2014-05-27 16:40:10 +0200

    icinga2x: Fetch latest project files.

--- icinga2x/.vagrant-puppet/manifests/finalize.sh
+++ icinga2x/.vagrant-puppet/manifests/finalize.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-echo "The Icinga 2 Vagrant VM has finished installing. See http://localhost:8080/ for more details."
+echo "The Icinga 2 Vagrant VM has finished installing. See http://localhost:8080 for more details."
