f5d8d621f3a3ed3b2e08093c966eed25e6743c3e
Michael Friedrich <michael.friedrich@netways.de>
2014-07-21 17:32:04 +0200

    graphite: Fix default http port, restart httpd for graphite wscgi problem

--- graphite/.vagrant-puppet/manifests/finalize.sh
+++ graphite/.vagrant-puppet/manifests/finalize.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
-echo "The Graphite Vagrant VM has finished installing. See http://localhost:8081/ for more details."
+echo "Sleep 5 seconds and restart httpd for graphite."
+sleep 5
+/etc/init.d/httpd restart
+
+echo "The Graphite Vagrant VM has finished installing. See http://localhost:8090/ for more details."
 
