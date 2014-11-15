2af23fb4d13b25f70246a79682a982b7471fc4d3
Michael Friedrich <michael.friedrich@netways.de>
2014-11-15 13:06:50 +0100

    icinga2x*: Fix icingaweb2 user provisioning

--- icinga2x-cluster/.vagrant-puppet/modules/icingaweb2/manifests/init.pp
+++ icinga2x-cluster/.vagrant-puppet/modules/icingaweb2/manifests/init.pp
@@ -36,11 +36,11 @@ class icingaweb2-internal-db-mysql {
     unless  => 'mysql -uicingaweb -picingaweb icingaweb -e "SELECT * FROM icingaweb_user;" &> /dev/null',
     command => 'mysql -uicingaweb -picingaweb icingaweb < /usr/share/doc/icingaweb2-$(rpm -q icingaweb2 | cut -d\'-\' -f2)/schema/mysql.schema.sql',
     require => [ Exec['create-mysql-icingaweb-db'], Package['icingaweb2'] ]
-  } ->
+  }
+
   exec { 'populate-icingaweb-mysql-db-create-default-account':
     path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless  => 'mysql -uicingaweb -picingaweb icingaweb -e "SELECT * FROM icingaweb_user;" &> /dev/null',
-    command => 'mysql -uicingaweb -picingaweb icingaweb -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'$1$iQSrnmO9$T3NVTu0zBkfuim4lWNRmH.\');"',
+    command => 'mysql -uicingaweb -picingaweb icingaweb -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
     require => [ Exec['populate-icingaweb-mysql-db'], Package['icingaweb2'] ]
   }
 }
