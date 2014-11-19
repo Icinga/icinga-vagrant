b1ec8d022b7a97623d4d08295f402b11c2eb0926
Bernd Ahlers <bernd@torch.sh>
2014-11-19 01:48:51 +0100

    icinga2x-graylog2: Move finalize script and adjust output.

--- icinga2x-graylog2/manifests/finalize.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo "The Graylog2 & Icinga 2 Vagrant VM has finished installing. Log into http://localhost:9000 (admin/admin) or http://localhost:8110/icinga (icingaadmin/icingaadmin)"
---
+++ icinga2x-graylog2/scripts/finalize.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cat <<__MESSAGE
+###############################################################################
+
+The Graylog2 & Icinga 2 Vagrant VM has finished installing.
+
+Available web applications:
+
+* Graylog2: http://localhost:9000 (admin/admin)
+* Icinga 2: http://localhost:8110/icinga (icingaadmin/icingaadmin)
+
+###############################################################################
+__MESSAGE
