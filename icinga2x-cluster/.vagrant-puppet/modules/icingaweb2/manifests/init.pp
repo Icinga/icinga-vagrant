420203a4f2480e7d1d363fa755655af2fd597f49
Michael Friedrich <michael.friedrich@netways.de>
2015-01-13 11:47:11 +0100

    icinga2x-cluster: Import icingaweb2 fixes

--- icinga2x-cluster/.vagrant-puppet/modules/icingaweb2/manifests/init.pp
+++ icinga2x-cluster/.vagrant-puppet/modules/icingaweb2/manifests/init.pp
@@ -19,7 +19,8 @@ class icingaweb2 {
   }
 
   package { ['php-ZendFramework', 'php-ZendFramework-Db-Adapter-Pdo-Mysql']:
-    ensure => latest
+    ensure => latest,
+    require => Class['icinga-rpm-snapshot']
   }
   
   file { '/etc/icingaweb2':
@@ -102,17 +103,17 @@ class icingaweb2 {
 }
 
 class icingaweb2-internal-db-mysql {
-  exec { 'create-mysql-icingaweb-db':
+  exec { 'create-mysql-icingaweb2-db':
     path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless  => 'mysql -uicingaweb -picingaweb icingaweb',
-    command => 'mysql -uroot -e "CREATE DATABASE icingaweb; GRANT ALL ON icingaweb.* TO icingaweb@localhost IDENTIFIED BY \'icingaweb\';"',
-    require => Service['mysqld']
+    unless  => 'mysql -uicingaweb2 -picingaweb2 icingaweb2',
+    command => 'mysql -uroot -e "CREATE DATABASE icingaweb2; GRANT ALL ON icingaweb2.* TO icingaweb2@localhost IDENTIFIED BY \'icingaweb2\';"',
+    require => Service['mariadb']
   }
 
-  exec { 'populate-icingaweb-mysql-db':
+  exec { 'populate-icingaweb2-mysql-db':
     path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless  => 'mysql -uicingaweb -picingaweb icingaweb -e "SELECT * FROM icingaweb_user;" &> /dev/null',
-    command => 'mysql -uicingaweb -picingaweb icingaweb < /usr/share/doc/icingaweb2/schema/mysql.schema.sql; mysql -uicingaweb -picingaweb icingaweb -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
-    require => [ Exec['create-mysql-icingaweb-db'], Package['icingaweb2'] ]
+    unless  => 'mysql -uicingaweb2 -picingaweb2 icingaweb2 -e "SELECT * FROM icingaweb_user;" &> /dev/null',
+    command => 'mysql -uicingaweb2 -picingaweb2 icingaweb2 < /usr/share/doc/icingaweb2/schema/mysql.schema.sql; mysql -uicingaweb2 -picingaweb2 icingaweb2 -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
+    require => [ Exec['create-mysql-icingaweb2-db'], Package['icingaweb2'] ]
   }
 }
