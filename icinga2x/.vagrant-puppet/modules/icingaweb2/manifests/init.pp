9e30dcd7a99524a3e21691f876df60bdedbd2531
Michael Friedrich <michael.friedrich@netways.de>
2015-01-13 09:07:39 +0100

    icinga2x: Import possible fix for web2 modules

--- icinga2x/.vagrant-puppet/modules/icingaweb2/manifests/init.pp
+++ icinga2x/.vagrant-puppet/modules/icingaweb2/manifests/init.pp
@@ -105,15 +105,15 @@ class icingaweb2 {
 class icingaweb2-internal-db-mysql {
   exec { 'create-mysql-icingaweb-db':
     path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless  => 'mysql -uicingaweb -picingaweb icingaweb',
-    command => 'mysql -uroot -e "CREATE DATABASE icingaweb; GRANT ALL ON icingaweb.* TO icingaweb@localhost IDENTIFIED BY \'icingaweb\';"',
+    unless  => 'mysql -uicingaweb2 -picingaweb2 icingaweb2',
+    command => 'mysql -uroot -e "CREATE DATABASE icingaweb2; GRANT ALL ON icingaweb2.* TO icingaweb2@localhost IDENTIFIED BY \'icingaweb2\';"',
     require => Service['mariadb']
   }
 
   exec { 'populate-icingaweb-mysql-db':
     path => '/bin:/usr/bin:/sbin:/usr/sbin',
-    unless  => 'mysql -uicingaweb -picingaweb icingaweb -e "SELECT * FROM icingaweb_user;" &> /dev/null',
-    command => 'mysql -uicingaweb -picingaweb icingaweb < /usr/share/doc/icingaweb2-$(rpm -q icingaweb2 | cut -d\'-\' -f2)/schema/mysql.schema.sql; mysql -uicingaweb -picingaweb icingaweb -e "INSERT INTO icingaweb_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
+    unless  => 'mysql -uicingaweb2 -picingaweb2 icingaweb2 -e "SELECT * FROM icingaweb_user;" &> /dev/null',
+    command => 'mysql -uicingaweb2 -picingaweb2 icingaweb2 < /usr/share/icingaweb2/schema/mysql.schema.sql; mysql -uicingaweb2 -picingaweb2 icingaweb2 -e "INSERT INTO icingaweb2_user (name, active, password_hash) VALUES (\'icingaadmin\', 1, \'\$1\$iQSrnmO9\$T3NVTu0zBkfuim4lWNRmH.\');"',
     require => [ Exec['create-mysql-icingaweb-db'], Package['icingaweb2'] ]
   }
 }
