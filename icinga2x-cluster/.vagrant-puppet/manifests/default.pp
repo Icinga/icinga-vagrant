8c5b64aee11829a8b7eced05d7b66df7ff2874c0
Michael Friedrich <michael.friedrich@gmail.com>
2015-02-08 16:45:36 +0100

    icinga2x*: Present icinga2 docs as module in icingaweb2
    fixes #7637

--- icinga2x-cluster/.vagrant-puppet/manifests/default.pp
+++ icinga2x-cluster/.vagrant-puppet/manifests/default.pp
@@ -95,6 +95,24 @@ file { '/usr/lib64/nagios/plugins/check_snmp_int.pl':
 # Icinga 2 General
 ####################################
 
+# present icinga2 in icingaweb2's module documentation
+file { '/usr/share/icingaweb2/modules/icinga2':
+  ensure => 'directory',
+  require => Package['icingaweb2']
+}
+
+file { '/usr/share/icingaweb2/modules/icinga2/doc':
+  ensure => 'link',
+  target => '/usr/share/doc/icinga2/markdown',
+  require => [ Package['icinga2'], Package['icingaweb2'], File['/usr/share/icingaweb2/modules/icinga2'] ],
+}
+
+file { '/etc/icingaweb2/enabledModules/icinga2':
+  ensure => 'link',
+  target => '/usr/share/icingaweb2/modules/icinga2',
+  require => File['/etc/icingaweb2/enabledModules'],
+}
+
 user { 'vagrant':
   groups  => 'icingacmd',
   require => Package['icinga2']
--- icinga2x/.vagrant-puppet/manifests/default.pp
+++ icinga2x/.vagrant-puppet/manifests/default.pp
@@ -85,3 +85,23 @@ exec { 'copy-vim-ftdetect-file':
 
 # enable the command pipe
 icinga2::feature { 'command': }
+
+
+# present icinga2 in icingaweb2's module documentation
+file { '/usr/share/icingaweb2/modules/icinga2':
+  ensure => 'directory',
+  require => Package['icingaweb2']
+}
+
+file { '/usr/share/icingaweb2/modules/icinga2/doc':
+  ensure => 'link',
+  target => '/usr/share/doc/icinga2/markdown',
+  require => [ Package['icinga2'], Package['icingaweb2'], File['/usr/share/icingaweb2/modules/icinga2'] ],
+}
+
+file { '/etc/icingaweb2/enabledModules/icinga2':
+  ensure => 'link',
+  target => '/usr/share/icingaweb2/modules/icinga2',
+  require => File['/etc/icingaweb2/enabledModules'],
+}
+
