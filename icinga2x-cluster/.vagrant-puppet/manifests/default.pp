7ad9d7d0c244b5daa400e72ebf68135fe8f50476
Michael Friedrich <michael.friedrich@gmail.com>
2015-02-08 15:58:10 +0100

    icinga2x*: Fix puppet hiera warnings

--- icinga2x-cluster/.vagrant-puppet/manifests/default.pp
+++ icinga2x-cluster/.vagrant-puppet/manifests/default.pp
@@ -15,9 +15,13 @@ include selinux
 ####################################
 # Basic stuff
 ####################################
-
-Package {
-  allow_virtual => false
+# fix puppet warning.
+# https://ask.puppetlabs.com/question/6640/warning-the-package-types-allow_virtual-parameter-will-be-changing-its-default-value-from-false-to-true-in-a-future-release/
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
 }
 
 package { [ 'vim-enhanced', 'mailx', 'tree', 'gdb' ]:
