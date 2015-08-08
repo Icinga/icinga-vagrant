6e14e527031667caa811ad8e26a8b4e0f4b4f24d
Bernd Ahlers <bernd@graylog.com>
2015-08-08 12:36:50 +0200

    icinga2x-graylog2: Fix Graylog URL in finalize.sh

--- icinga2x-graylog2/scripts/finalize.sh
+++ icinga2x-graylog2/scripts/finalize.sh
@@ -3,11 +3,11 @@
 cat <<__MESSAGE
 ###############################################################################
 
-The Graylog2 & Icinga 2 Vagrant VM has finished installing.
+The Graylog & Icinga 2 Vagrant VM has finished installing.
 
 Available web applications:
 
-* Graylog2: http://192.168.33.6 (admin/admin)
+* Graylog:  http://192.168.33.6:9000/ (admin/admin)
 * Icinga 2: http://192.168.33.6/icingaweb2 (icingaadmin/icinga)
 
 ###############################################################################
