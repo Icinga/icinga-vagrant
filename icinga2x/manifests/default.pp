57770c07c84ed1d29817e044bbb905b7a3037414
Michael Friedrich <michael.friedrich@netways.de>
2016-02-17 00:36:23 +0100

    icinga2x: Add icingaweb2-module-graphite

--- icinga2x/manifests/default.pp
+++ icinga2x/manifests/default.pp
@@ -332,7 +332,7 @@ file { 'nagvis-core-functions-index.php':
 }
 
 ####################################
-# Graphite
+# Dashing
 ####################################
 
 package { [ 'rubygems', 'rubygem-bundler', 'ruby-devel', 'openssl', 'gcc-c++', 'make', 'nodejs' ]:
@@ -454,6 +454,34 @@ class { 'graphite':
 #  source	=> 'puppet:///vagrant/files/opt/graphite/webapp/content/js/composer_widgets.js',
 #  require	=> Class['graphite'],
 #}
+
+# icingaweb2 module
+icingaweb2::module { 'graphite':
+  builtin => false
+}
+
+# ship box specific graphite module config
+file {'/etc/icingaweb2/modules/graphite':
+  ensure => directory,
+  owner  => root,
+  group  => icingaweb2,
+  mode => '2770',
+  require => [ Package['icingaweb2'], File['/etc/icingaweb2/modules'] ]
+} ->
+file { '/etc/icingaweb2/modules/graphite/templates':
+  ensure => link,
+  force => true,
+  target => '/usr/share/icingaweb2/modules/graphite/sample-config/icinga2/templates'
+} ->
+file { '/etc/icingaweb2/modules/graphite/config.ini':
+  ensure => file,
+  owner => root,
+  group => icingaweb2,
+  mode => '2770',
+  source => 'puppet:////vagrant/files/etc/icingaweb2/modules/graphite/config.ini', #TODO use hiera and templates
+  require => File['/etc/icingaweb2/modules/graphite']
+}
+
 
 ####################################
 # Grafana
