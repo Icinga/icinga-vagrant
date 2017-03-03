77eb9f8c58b300fb561ab6a07fdfd6dfba2cc9eb
Michael Friedrich <michael.friedrich@icinga.com>
2017-03-03 17:03:45 +0100

    icinga2x: Update Grafana to 4.1.2

--- icinga2x/manifests/default.pp
+++ icinga2x/manifests/default.pp
@@ -604,10 +604,10 @@ file { '/etc/icingaweb2/modules/graphite/config.ini':
 ####################################
 
 # https://github.com/bfraser/puppet-grafana
-# https://grafanarel.s3.amazonaws.com/builds/grafana-3.1.1-1470047149.x86_64.rpm
+# https://grafanarel.s3.amazonaws.com/builds/grafana-4.1.2-1486989747.x86_64.rpm
 class { 'grafana':
-  version => '3.1.1',
-  rpm_iteration => '1470047149',
+  version => '4.1.2',
+  rpm_iteration => '1486989747',
   cfg => {
     app_mode => 'production',
     server   => {
