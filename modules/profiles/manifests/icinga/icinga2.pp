43e9507a69100d6bec5364b721cd3dc669c00e59
Michael Friedrich <michael.friedrich@icinga.com>
2017-11-16 22:06:14 +0100

    Puppet: Use mysql::db for profiles::icinga::icinga2
    Woah, what a mess.

--- modules/profiles/manifests/icinga/icinga2.pp
+++ modules/profiles/manifests/icinga/icinga2.pp
@@ -1,7 +1,37 @@
 class profiles::icinga::icinga2 {
   include icinga2 # TODO: Use official module with Puppet 5 support
-  include icinga2_ido_mysql # TODO: Refactor module
   include monitoring_plugins # TODO: Refactor module
+
+  include '::profiles::base::system'
+
+  package { 'icinga2-ido-mysql':
+    ensure => latest,
+    require => Class['icinga_rpm'],
+    alias => 'icinga2-ido-mysql'
+  }
+
+  # TODO: remove hardcoded names
+  mysql::db { 'icinga':
+    user      => 'icinga',
+    password  => 'icinga',
+    host      => 'localhost',
+    charset   => 'latin1',
+    collate   => 'latin1_general_ci',
+    grant     => [ 'ALL' ]
+  }
+
+  exec { 'populate-icinga2-ido-mysql-db':
+    path => '/bin:/usr/bin:/sbin:/usr/sbin',
+    unless => "mysql -uicinga -picinga icinga -e \"SELECT * FROM icinga_dbversion;\" &> /dev/null",
+    command => "mysql -uicinga -picinga icinga < /usr/share/icinga2-ido-mysql/schema/mysql.sql",
+    user => 'root',
+    environment => [ "HOME=/root" ],
+    require => [ Mysql::Db['icinga'], Package['icinga2-ido-mysql'] ]
+  }
+
+  icinga2::feature { 'ido-mysql':
+    require => Exec['populate-icinga2-ido-mysql-db']
+  }
 
   package { 'vim-icinga2':
     ensure => 'latest',
