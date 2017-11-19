daba924ec3f598a0957ef3c06ca44dd63ff0005d
Michael Friedrich <michael.friedrich@icinga.com>
2017-11-19 23:37:23 +0100

    icinga2x-influxdb: Refactor code and cleanup
    Icinga Web 2 modules still missing, Grafana/InfluxDB profile too.

--- icinga2x-influxdb/manifests/default.pp
+++ icinga2x-influxdb/manifests/default.pp
@@ -1,216 +1,52 @@
-include icinga_rpm
-include epel
-include icinga2
-include icinga2_ido_mysql
-include icingaweb2
-include icingaweb2_internal_db_mysql
-include monitoring_plugins
-
-class { 'selinux':
-  mode => 'disabled'
-}
 
 ####################################
-# Database
+# Global configuration
 ####################################
 
-$mysql_server_override_options = {}
-
-class { '::mysql::server':
-  root_password => 'icingar0xx',
-  remove_default_accounts => true,
-  override_options => $mysql_server_override_options
-}
+$hostOnlyIP = '192.168.33.8'
+$hostOnlyFQDN = 'icinga2x-influxdb.vagrant.demo.icinga.com'
 
 ####################################
-# Webserver
+# Setup
 ####################################
 
-class {'apache':
-  # don't purge php, icingaweb2, etc configs
-  purge_configs => false,
-  default_vhost => false
+class { '::profiles::base::system': }
+->
+class { '::profiles::base::mysql': }
+->
+class { '::profiles::base::apache': }
+->
+class { '::profiles::icinga::icinga2': }
+->
+class { '::profiles::icinga::icingaweb2':
+  icingaweb2_listen_ip => $hostOnlyIP,
+  icingaweb2_fqdn => $hostOnlyFQDN
 }
 
-class {'::apache::mod::php': }
 
-apache::vhost { 'vagrant-demo.icinga.com':
-  priority        => 5,
-  port            => '80',
-  docroot         => '/var/www/html',
-  rewrites => [
-    {
-      rewrite_rule => ['^/$ /icingaweb2 [NE,L,R=301]'],
-    },
-  ],
-}
-
-apache::vhost { 'vagrant-demo.icinga.com-ssl':
-  priority        => 5,
-  port            => '443',
-  docroot         => '/var/www/html',
-  ssl		  => true,
-  add_listen      => false, #prevent duplicate listen entries
-  rewrites => [
-    {
-      rewrite_rule => ['^/$ /icingaweb2 [NE,L,R=301]'],
-    },
-  ],
-}
-include '::php::cli'
-include '::php::mod_php5'
-
-php::ini { '/etc/php.ini':
-  display_errors => 'On',
-  memory_limit => '256M',
-  date_timezone => 'Europe/Berlin',
-  session_save_path => '/var/lib/php/session'
-}
-
-####################################
-# Misc
-####################################
-# fix puppet warning.
-# https://ask.puppetlabs.com/question/6640/warning-the-package-types-allow_virtual-parameter-will-be-changing-its-default-value-from-false-to-true-in-a-future-release/
-if versioncmp($::puppetversion,'3.6.1') >= 0 {
-  $allow_virtual_packages = hiera('allow_virtual_packages',false)
-  Package {
-    allow_virtual => $allow_virtual_packages,
-  }
-}
-
-package { [ 'mailx', 'tree', 'gdb', 'git', 'bash-completion', 'screen', 'htop' ]:
-  ensure => 'installed',
-  require => Class['epel']
-}
-
-@user { vagrant: ensure => present }
-User<| title == vagrant |>{
-  groups +> ['icinga', 'icingacmd'],
-  require => Package['icinga2']
-}
-
-file { '/etc/motd':
-  source => 'puppet:////vagrant/files/etc/motd',
-  owner => root,
-  group => root
-}
-
-file { '/etc/profile.d/env.sh':
-  source => 'puppet:////vagrant/files/etc/profile.d/env.sh'
-}
-
-# Required by vim-icinga2
-class { 'vim':
-  opt_bg_shading => 'light',
-}
-
-####################################
-# Icinga 2 General
-####################################
-
-file { '/etc/icinga2':
-  ensure  => 'directory',
-  require => Package['icinga2']
-}
-
-file { '/etc/icinga2/icinga2.conf':
-  owner  => icinga,
-  group  => icinga,
-  source    => "puppet:////vagrant/files/etc/icinga2/icinga2.conf",
-  require   => File['/etc/icinga2'],
-  notify    => Service['icinga2']
-}
-
-file { "/etc/icinga2/zones.conf":
-  owner  => icinga,
-  group  => icinga,
-  source    => "puppet:////vagrant/files/etc/icinga2/zones.conf",
-  require   => Package['icinga2'],
-  notify    => Service['icinga2']
-}
-
-file { '/etc/icinga2/conf.d/hosts.conf':
-  owner  => icinga,
-  group  => icinga,
-  source    => 'puppet:////vagrant/files/etc/icinga2/conf.d/hosts.conf',
-  require   => Package['icinga2'],
-  notify    => Service['icinga2']
-}
-
-file { '/etc/icinga2/conf.d/additional_services.conf':
-  owner  => icinga,
-  group  => icinga,
-  source    => 'puppet:////vagrant/files/etc/icinga2/conf.d/additional_services.conf',
-  require   => Package['icinga2'],
-  notify    => Service['icinga2']
-}
-
-# api
-exec { 'enable-icinga2-api':
-  path => '/bin:/usr/bin:/sbin:/usr/sbin',
-  command => 'icinga2 api setup',
-  require => Package['icinga2'],
-  notify  => Service['icinga2']
-}
-
-file { '/etc/icinga2/conf.d/api-users.conf':
-  owner  => icinga,
-  group  => icinga,
-  content   => template("icinga2/api-users.conf.erb"),
-  require   => [ Package['icinga2'], Exec['enable-icinga2-api'] ],
-  notify    => Service['icinga2']
-}
-
-file { 'check_mysql_health':
-  name => '/usr/lib64/nagios/plugins/check_mysql_health',
-  owner => root,
-  group => root,
-  mode => '0755',
-  source    => 'puppet:////vagrant/files/usr/lib64/nagios/plugins/check_mysql_health',
-  require => Class['monitoring_plugins'],
-}
 
 ####################################
 # Icinga Web 2
 ####################################
 
-# user-defined preferences (using the iframe module)
-file { '/etc/icingaweb2/preferences':
-  ensure => directory,
-  owner  => root,
-  group  => icingaweb2,
-  mode => '2770',
-  require => Package['icingaweb2']
-}
-
-file { '/etc/icingaweb2/preferences/icingaadmin':
-  ensure => directory,
-  recurse => true,
-  owner  => root,
-  group  => icingaweb2,
-  mode => '2770',
-  source    => "puppet:////vagrant/files/etc/icingaweb2/preferences/icingaadmin",
-  require => [ Package['icingaweb2'], File['/etc/icingaweb2/preferences'] ]
-}
-
-# present icinga2 in icingaweb2's module documentation
-file { '/usr/share/icingaweb2/modules/icinga2':
-  ensure => 'directory',
-  require => Package['icingaweb2']
-}
-
-file { '/usr/share/icingaweb2/modules/icinga2/doc':
-  ensure => 'link',
-  target => '/usr/share/doc/icinga2/markdown',
-  require => [ Package['icinga2'], Package['icingaweb2'], File['/usr/share/icingaweb2/modules/icinga2'] ],
-}
-
-file { '/etc/icingaweb2/enabledModules/icinga2':
-  ensure => 'link',
-  target => '/usr/share/icingaweb2/modules/icinga2',
-  require => File['/etc/icingaweb2/enabledModules'],
-}
+## user-defined preferences (using the iframe module)
+#file { '/etc/icingaweb2/preferences':
+#  ensure => directory,
+#  owner  => root,
+#  group  => icingaweb2,
+#  mode => '2770',
+#  require => Package['icingaweb2']
+#}
+#
+#file { '/etc/icingaweb2/preferences/icingaadmin':
+#  ensure => directory,
+#  recurse => true,
+#  owner  => root,
+#  group  => icingaweb2,
+#  mode => '2770',
+#  source    => "puppet:////vagrant/files/etc/icingaweb2/preferences/icingaadmin",
+#  require => [ Package['icingaweb2'], File['/etc/icingaweb2/preferences'] ]
+#}
 
 ####################################
 # InfluxDB
