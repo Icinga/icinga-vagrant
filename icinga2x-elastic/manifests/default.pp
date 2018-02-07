a065ec1f18332bf67e0fdc5dbe8b0b820f83d151
Michael Friedrich <michael.friedrich@icinga.com>
2018-02-07 18:16:41 +0100

    Elastic box: Finalize manifest for 6.x

--- icinga2x-elastic/manifests/default.pp
+++ icinga2x-elastic/manifests/default.pp
@@ -6,24 +6,19 @@ $nodeName = 'icinga2-elastic' # TODO: Hiera.
 $hostOnlyIP = '192.168.33.7'
 $hostOnlyFQDN = 'icinga2x-elastic.vagrant.demo.icinga.com'
 
-# Elastic
-# TODO: Wait for 6.x support in Icingabeat
-#$elasticRepoVersion = '6.x'
-#$elasticsearchVersion = '6.0.0'
-#$kibanaVersion = '6.0.0'
-#$icingabeatVersion = '1.1.1'
-#$icingabeatDashboardsChecksum = '11f1f92e541f4256727137094d4d69efdd6f3862'
-$elasticRepoVersion = '5.x'
-$elasticsearchVersion = '5.6.4'
-$kibanaVersion = '5.3.1'
-$icingabeatVersion = '1.1.0'
-$icingabeatDashboardsChecksum = '9c98cf4341cbcf6d4419258ebcc2121c3dede020'
-# keep this in sync with the icingabeat dashboard ids!
-# http://192.168.33.7:5601/app/kibana#/dashboard/720f2f20-0979-11e7-a4dd-e96fa284b426
-$kibanaDefaultAppId = 'dashboard/720f2f20-0979-11e7-a4dd-e96fa284b426'
-
 $elasticsearchListenIP = 'localhost'
 $elasticsearchListenPort = '9200'
+$kibanaListenIP = 'localhost'
+$kibanaListenPort = 5601
+
+# Elastic
+$elasticRepoVersion = '6.x'
+$elasticsearchVersion = '6.2.0'
+$kibanaVersion = '6.2.0'
+$icingabeatVersion = '6.1.1'
+# keep this in sync with the icingabeat dashboard ids!
+# http://192.168.33.7:5601/app/kibana#/dashboard/34e97340-e4ce-11e7-b4d1-8383451ae5a4
+$kibanaDefaultAppId = 'dashboard/34e97340-e4ce-11e7-b4d1-8383451ae5a4'
 
 ####################################
 # Setup
@@ -38,7 +33,13 @@ class { '::profiles::base::apache': }
 class { '::profiles::base::java': }
 ->
 class { '::profiles::icinga::icinga2':
-  features => [ "elasticsearch" ]
+  node_name => $nodeName,
+  features => {
+    "elasticsearch" => {
+      "listen_ip"   => $elasticsearchListenIP,
+      "listen_port" => $elasticsearchListenPort
+    },
+  }
 }
 ->
 class { '::profiles::icinga::icingaweb2':
@@ -48,19 +49,20 @@ class { '::profiles::icinga::icingaweb2':
     "elasticsearch" => {
       "listen_ip"   => $elasticsearchListenIP,
       "listen_port" => $elasticsearchListenPort
-    }
+    },
   }
 }
 ->
 class { '::profiles::elastic::elasticsearch':
   repo_version => $elasticRepoVersion,
+  elasticsearch_revision => $elasticsearchVersion
 }
 ->
 class { '::profiles::elastic::kibana':
   repo_version => $elasticRepoVersion,
   kibana_revision => "${kibanaVersion}-1",
-  kibana_host => '127.0.0.1',
-  kibana_port => 5601,
+  kibana_host => $kibanaListenIP,
+  kibana_port => $kibanaListenPort,
   kibana_default_app_id => $kibanaDefaultAppId
 }
 ->
@@ -70,12 +72,14 @@ class { '::profiles::elastic::httpproxy':
 }
 ->
 class { '::profiles::elastic::filebeat':
-  filebeat_major_version => '5'
+  filebeat_major_version => '6'
 }
 ->
 class { '::profiles::elastic::icingabeat':
   icingabeat_version => $icingabeatVersion,
-  icingabeat_dashboards_checksum => $icingabeatDashboardsChecksum,
-  kibana_version => $kibanaVersion
+  kibana_version => $kibanaVersion,
+  elasticsearch_host => $elasticsearchListenIP,
+  elasticsearch_port => $elaticsearchListenPort,
+  kibana_host => $kibanaListenIP, # needed for dashboard provisioning
+  kibana_port => $kibanaListenPort,
 }
-
