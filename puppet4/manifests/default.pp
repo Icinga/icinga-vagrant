c56ecbadbe7fdd20cb5853a0ccc70fa3c8b6c1ba
Michael Friedrich <michael.friedrich@icinga.com>
2018-02-08 18:34:23 +0100

    Puppet4: Update demo environment

--- puppet4/manifests/default.pp
+++ puppet4/manifests/default.pp
@@ -6,8 +6,10 @@ $nodeName = 'icinga2-puppet4' # TODO: Hiera.
 $hostOnlyIP = '192.168.33.50'
 $hostOnlyFQDN = 'puppet4.vagrant.demo.icinga.com'
 
-$graphiteListenIP = $hostOnlyIP
-$graphiteListenPort = 8003
+$graphiteWebListenIP = $hostOnlyIP
+$graphiteWebListenPort = 8003
+$carbonListenIP = $hostOnlyIP
+$carbonListenPort = 2003
 $influxdbListenIP = $hostOnlyIP
 $influxdbListenPort = 8086
 $grafanaListenIP = $hostOnlyIP
@@ -21,6 +23,8 @@ $gelfListenPort = 12201
 $graylogListenIP = $hostOnlyIP
 $graylogListenPort = 9000
 
+# Icinga
+$directorVersion = '1.4.1'
 
 # Elastic
 $elasticRepoVersion = '6.x'
@@ -43,57 +47,61 @@ class { '::profiles::base::apache': }
 ->
 class { '::profiles::base::java': }
 ->
-class { '::profiles::icinga::icinga2':
-  node_name => $nodeName,
-  features => {
-    #"graphite" => {
-    #  "listen_ip"   => $graphiteListenIP,
-    #  "listen_port" => $graphiteListenPort
-    #},
-    #"influxdb" => {
-    #  "listen_ip"   => $influxdbListenIP,
-    #  "listen_port" => $influxdbListenPort
-    #},
-    #"gelf" => {
-    #  "listen_ip"   => $gelfListenIP,
-    #  "listen_port" => $gelfListenPort
-    #}
-    "elasticsearch" => {
-      "listen_ip"   => $elasticsearchListenIP,
-      "listen_port" => $elasticsearchListenPort
-    },
-  }
-}
-->
-class { '::profiles::icinga::icingaweb2':
-  icingaweb2_listen_ip => $hostOnlyIP,
-  icingaweb2_fqdn => $hostOnlyFQDN,
-  modules => {
-#    "grafana" => {
-#      "datasource"  => "graphite",
-#      "listen_ip"   => $hostOnlyIP,
-#      "listen_port" => $grafanaListenPort
+#class { '::profiles::icinga::icinga2':
+#  node_name => $nodeName,
+#  features => {
+#    #"graphite" => {
+#    #  "listen_ip"   => $carbonListenIP,
+#    #  "listen_port" => $carbonListenPort
+#    #},
+#    #"influxdb" => {
+#    #  "listen_ip"   => $influxdbListenIP,
+#    #  "listen_port" => $influxdbListenPort
+#    #},
+#    #"gelf" => {
+#    #  "listen_ip"   => $gelfListenIP,
+#    #  "listen_port" => $gelfListenPort
+#    #}
+#    "elasticsearch" => {
+#      "listen_ip"   => $elasticsearchListenIP,
+#      "listen_port" => $elasticsearchListenPort
 #    },
-#    "map" => {},
-    "elasticsearch" => {
-      "listen_ip"   => $elasticsearchListenIP,
-      "listen_port" => $elasticsearchListenPort
-    },
-#    "graphite" => {
-#      "listen_ip"   => $graphiteListenIP,
-#      "listen_port" => $graphiteListenPort
-#    },
-#    "graylog" => {
-#      "listen_ip"   => $graylogListenIP,
-#      "listen_port" => $graylogListenPort
-#    }
-  }
-}
-#->
-#class { '::profiles::graphite::server':
-#  listen_ip   => $graphiteListenIP,
-#  listen_port => $graphiteListenPort
+#  }
 #}
+#->
+#class { '::profiles::icinga::icingaweb2':
+#  icingaweb2_listen_ip => $hostOnlyIP,
+#  icingaweb2_fqdn => $hostOnlyFQDN,
+#  node_name => $nodeName,
+#  modules => {
+##    "grafana" => {
+##      "datasource"  => "graphite",
+##      "listen_ip"   => $grafanaListenIP,
+##      "listen_port" => $grafanaListenPort
+##    },
+##    "map" => {},
+#    "director" => {
+#      "git_revision" => $directorVersion
+#    },
+#    "elasticsearch" => {
+#      "listen_ip"   => $elasticsearchListenIP,
+#      "listen_port" => $elasticsearchListenPort
+#    },
+##    "graphite" => {
+##      "listen_ip"   => $graphiteWebListenIP,
+##      "listen_port" => $graphiteWebListenPort
+##    },
+##    "graylog" => {
+##      "listen_ip"   => $graylogListenIP,
+##      "listen_port" => $graylogListenPort
+##    }
+#  }
+#}
+#->
+class { '::profiles::graphite::server':
+  listen_ip   => $graphiteListenIP,
+  listen_port => $graphiteListenPort
+}
 #->
 #class { '::profiles::dashing::icinga2': }
 #->
@@ -113,43 +121,43 @@ class { '::profiles::icinga::icingaweb2':
 #}
 #->
 #class { '::profiles::grafana::server':
-#  listen_ip => $hostOnlyIP,
+#  listen_ip => $grafanaListenIP,
 #  listen_port => $grafanaListenPort,
 #  version => '4.6.2-1',
 #  backend => "graphite",
 #  backend_port => $graphiteListenPort
 #}
-->
-class { '::profiles::elastic::elasticsearch':
-  repo_version => $elasticRepoVersion,
-  elasticsearch_revision => $elasticsearchVersion
-}
-->
-class { '::profiles::elastic::kibana':
-  repo_version => $elasticRepoVersion,
-  kibana_revision => "${kibanaVersion}-1",
-  kibana_host => $kibanaListenIP,
-  kibana_port => $kibanaListenPort,
-  kibana_default_app_id => $kibanaDefaultAppId
-}
-->
-class { '::profiles::elastic::httpproxy':
-  listen_ip => $hostOnlyIP,
-  node_name => $nodeName
-}
-->
-class { '::profiles::elastic::filebeat':
-  filebeat_major_version => '6'
-}
-->
-class { '::profiles::elastic::icingabeat':
-  icingabeat_version => $icingabeatVersion,
-  kibana_version => $kibanaVersion,
-  elasticsearch_host => $elasticsearchListenIP,
-  elasticsearch_port => $elaticsearchListenPort,
-  kibana_host => $kibanaListenIP, # needed for dashboard provisioning
-  kibana_port => $kibanaListenPort,
-}
+#->
+#class { '::profiles::elastic::elasticsearch':
+#  repo_version => $elasticRepoVersion,
+#  elasticsearch_revision => $elasticsearchVersion
+#}
+#->
+#class { '::profiles::elastic::kibana':
+#  repo_version => $elasticRepoVersion,
+#  kibana_revision => "${kibanaVersion}-1",
+#  kibana_host => $kibanaListenIP,
+#  kibana_port => $kibanaListenPort,
+#  kibana_default_app_id => $kibanaDefaultAppId
+#}
+#->
+#class { '::profiles::elastic::httpproxy':
+#  listen_ip => $hostOnlyIP,
+#  node_name => $nodeName
+#}
+#->
+#class { '::profiles::elastic::filebeat':
+#  filebeat_major_version => '6'
+#}
+#->
+#class { '::profiles::elastic::icingabeat':
+#  icingabeat_version => $icingabeatVersion,
+#  kibana_version => $kibanaVersion,
+#  elasticsearch_host => $elasticsearchListenIP,
+#  elasticsearch_port => $elaticsearchListenPort,
+#  kibana_host => $kibanaListenIP, # needed for dashboard provisioning
+#  kibana_port => $kibanaListenPort,
+#}
 
 #->
 #class { '::profiles::graylog::elasticsearch':
