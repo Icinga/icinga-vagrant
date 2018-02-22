38639f811b3deff4f5e75b8925f963bf9ff3920e
Michael Friedrich <michael.friedrich@icinga.com>
2018-02-22 19:54:06 +0100

    distributed: Refactor into Hiera and Puppet environments

--- .puppet/scripts/shell_provisioner_post.sh
+++ .puppet/scripts/shell_provisioner_post.sh
@@ -5,3 +5,6 @@ set -e
 if rpm -q "rh-php71-php-fpm" &>/dev/null; then
   systemctl restart rh-php71-php-fpm.service
 fi
+
+# workaround for https://github.com/Icinga/icinga2/issues/6112
+systemctl restart icinga2
