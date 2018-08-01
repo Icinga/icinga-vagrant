e7afef9388bbc7f1949afc056a3d79cc34aea5e9
Nicolai Buchwitz <nicolai.buchwitz@enda.eu>
2018-08-01 11:06:16 +0200

    Remove restart of icinga2

--- .puppet/scripts/shell_provisioner_post.sh
+++ .puppet/scripts/shell_provisioner_post.sh
@@ -5,8 +5,3 @@ set -e
 if rpm -q "rh-php71-php-fpm" &>/dev/null; then
   systemctl restart rh-php71-php-fpm.service
 fi
-
-if rpm -q "icinga2-bin" &>/dev/null; then
-  # workaround for https://github.com/Icinga/icinga2/issues/6112
-  systemctl restart icinga2
-fi
