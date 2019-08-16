2919b81701e3b251d5016f43c74138f6b4a586da
Michael Friedrich <michael.friedrich@icinga.com>
2019-08-16 10:25:18 +0200

    Puppet: Purge old selinux

--- .puppet/modules/selinux/lib/facter/selinux_semanage_is_python3.rb
+++
@@ -1,6 +0,0 @@
-Facter.add(:selinux_semanage_is_python3) do
-  confine osfamily: 'RedHat'
-  setcode do
-    Facter::Core::Execution.execute('rpm -q libsemanage-python3') !~ %r{not installed}
-  end
-end
