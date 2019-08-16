7963d2a47bfe3b50a826650ccbbcb6b6757e48a1
Michael Friedrich <michael.friedrich@icinga.com>
2019-08-16 10:35:43 +0200

    Puppet: Purge old graylog

--- .puppet/modules/graylog/manifests/init.pp
+++
@@ -1,3 +0,0 @@
-class graylog {
-  fail('Do not use the "graylog" class directly, use the "graylog::server" class!')
-}
--- .puppet/modules/graylog/spec/classes/init_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-describe 'graylog' do
-
-  context 'with default values for all parameters' do
-    it { should contain_class('graylog') }
-  end
-end
